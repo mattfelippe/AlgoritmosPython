639b84bc59551ea2c41782aa64cf3d84d08d6755
Matheus Felippe <43424878+mattfelippe@users.noreply.github.com>
2019-02-19 10:39:20 -0300

    Add files via upload

---
+++ Tarefa 13.py
@@ -0,0 +1,33 @@
+# Matheus Moreira -  desenhe  quadrados um dentro de outro
+import turtle
+
+from random import randint
+
+for count in range(4):
+    turtle.forward(200)
+    turtle.left(90)
+    
+for count in range(1):
+    turtle.penup()
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.pendown()
+    
+    
+for count in range(4):
+    turtle.forward(150)
+    turtle.right(90)
+    
+for count in range(1):
+    turtle.penup()
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.pendown()
+    
+for count in range(4):
+    turtle.forward(50)
+    turtle.left(90)
+    
+
