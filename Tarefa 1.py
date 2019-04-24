c6c796bbe568d17f8dd01570b43f56d51e7532a1
Matheus Felippe <43424878+mattfelippe@users.noreply.github.com>
2019-04-24 17:38:29 -0300

    Add files via upload

--- Tarefa 1.py
+++ Tarefa 1.py
@@ -1,11 +1,12 @@
-# Matheus Moreira - Retornar a soma de todos os números de 1 até N inclusive;
-n = int(input("Digite um número: "))
-inicio = 1
-total = 0
+#Tarefa 1 Matheus Moreira - Leia uma tupla contendo 3 números inteiros, (n1, n2, n3) e os imprima em ordem crescente.
 
-while(inicio <= n):
-    inicio += 2
-    total = total + inicio
+numero1 = int(input('Digite o primeiro número : '))
+numero2 = int(input('Digite o segundo número: '))
+numero3 = int(input('Digite o terceiro número: '))
 
-total = total - inicio
-print("Total:", total)
+tupla = (numero1, numero2, numero3)
+tupla_na_ordem_crescente = sorted(tupla)
+
+print('Tupla normal: ',tupla)
+print('')
+print('Tupla na ordem crescente: ',tupla_na_ordem_crescente)
